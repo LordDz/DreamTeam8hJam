d80cf76e2b45b62fb14216ac71787ad0e7b97737
David Zetterdahl <david.zetterdahl@hiq.se>
2020-02-29 14:48:08 +0100

    Game stuff too!

---
+++ EightHourJam/Assets/_Game/Scripts/Players/DamageType.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum DamageType22
+{
+    none,
+    bullet,
+    ray
+}
---
+++ EightHourJam/Assets/_Game/Scripts/UI/PlayerHealthImg.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHealthImg : MonoBehaviour
+{
+    public Image image;
+    public float height;
+
+    public void Damage(float originalHealth, float currentHealth)
+    {
+        //height -= damage;
+        image.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+    }
+}
